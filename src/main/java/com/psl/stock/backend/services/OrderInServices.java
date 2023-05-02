a0368307a1ef03fb2e0087284ca4309ac877c7cb
Ram Pujan Pandit <rampujandgp@gmail.com>
2023-05-02 18:16:42 +0530

    02/05/2023 1st commit

---
+++ src/main/java/com/psl/stock/backend/services/OrderInServices.java
@@ -0,0 +1,16 @@
+package com.psl.stock.backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.psl.stock.backend.repositories.OrderInRepo;
+
+@Service
+public class OrderInServices {
+
+    @Autowired
+    private OrderInRepo orderInRepo;
+
+    
+    
+}
